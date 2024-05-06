619fe3df45c478ec4bf13ad37cddc4ee711be128
Skizzle <74311119+Itspossible100pct@users.noreply.github.com>
2024-05-06 23:53:02 +0200

    Brough interactions prefabs over to Main Scene.  Next step flick action setup.

---
+++ Assets/Scripts/DistanceHandPhysicsInteractor.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using Oculus.Interaction.HandGrab;
+using UnityEngine;
+
+public class DistanceHandPhysicsInteractor : DistanceHandGrabInteractor
+{
+    // additional properties and methods for physics interaction
+    
+    protected override void DoSelectUpdate()
+    {
+        if (SelectedInteractable == null)
+        {
+            return;
+        }
+
+        // Instead of updating grab movement, apply physics interaction
+        ApplyPhysicsInteraction(SelectedInteractable);
+    }
+
+    private void ApplyPhysicsInteraction(DistanceHandGrabInteractable selectedInteractable)
+    {
+        // a collider will be attached to my middle finger with rigidbody
+        // when it flicks the interactable Ant (enemy npc), the ant goes flying
+        // Calculate force direction (towards the interactable)
+        // Apply force
+        Debug.Log("Ant goes flying!");
+    }
+    
+    
+    
+}
